18cdbf6a3e87db47ac68904ea01af5feb65c9682
gokulvasanthJ <gokul27mech@gmail.com>
2025-01-18 18:37:33 +0530

    dashboard removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
 import ReferralForm from './components/ReferralForm';
 import Navbar from './components/Navbar';
 import CandidateCard from './components/CandidateCard';
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard/>} /> {/* Add your Dashboard component */}
         <Route path="/refer" element={<ReferralForm />} /> {/* Ensure the ReferralForm component exists */}
         <Route path="/candidate-card" element={<CandidateCard />} />
         {/* Add more routes as necessary */}
